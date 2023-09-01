0e80c75ed984f4933c3101e0b8edf74dac1ea04b
Kimparkz <khimjype@gmail.com>
2023-09-01 12:20:52 +0700

    v.1.0.0 (alpha)

---
+++ TR-DDS by Kimparkz/Form1.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        TextBoxStatus = New TextBox()
+        TextBoxInputPath = New TextBox()
+        GroupBox1 = New GroupBox()
+        TextBox3 = New TextBox()
+        Label1 = New Label()
+        RadioButtonJPEG = New RadioButton()
+        RadioButtonPNG = New RadioButton()
+        ButtonBrowse = New Button()
+        ButtonConvert = New Button()
+        Label2 = New Label()
+        TextBoxOutputPath = New TextBox()
+        Label3 = New Label()
+        OpenFileDialog1 = New OpenFileDialog()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBoxStatus
+        ' 
+        TextBoxStatus.Location = New Point(16, 22)
+        TextBoxStatus.Multiline = True
+        TextBoxStatus.Name = "TextBoxStatus"
+        TextBoxStatus.ReadOnly = True
+        TextBoxStatus.Size = New Size(406, 175)
+        TextBoxStatus.TabIndex = 0
+        ' 
+        ' TextBoxInputPath
+        ' 
+        TextBoxInputPath.Location = New Point(79, 251)
+        TextBoxInputPath.Name = "TextBoxInputPath"
+        TextBoxInputPath.ReadOnly = True
+        TextBoxInputPath.Size = New Size(277, 23)
+        TextBoxInputPath.TabIndex = 1
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(RadioButtonJPEG)
+        GroupBox1.Controls.Add(RadioButtonPNG)
+        GroupBox1.Controls.Add(TextBoxStatus)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(438, 233)
+        GroupBox1.TabIndex = 3
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Process Box :"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Cursor = Cursors.Help
+        TextBox3.ForeColor = Color.Lime
+        TextBox3.Location = New Point(323, 203)
+        TextBox3.Name = "TextBox3"
+        TextBox3.ReadOnly = True
+        TextBox3.Size = New Size(109, 23)
+        TextBox3.TabIndex = 7
+        TextBox3.Text = "Discord : kimparkz"
+        TextBox3.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(16, 206)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(95, 15)
+        Label1.TabIndex = 4
+        Label1.Text = "Convert Option :"
+        ' 
+        ' RadioButtonJPEG
+        ' 
+        RadioButtonJPEG.AutoSize = True
+        RadioButtonJPEG.Cursor = Cursors.Help
+        RadioButtonJPEG.Enabled = False
+        RadioButtonJPEG.Location = New Point(167, 204)
+        RadioButtonJPEG.Name = "RadioButtonJPEG"
+        RadioButtonJPEG.Size = New Size(45, 19)
+        RadioButtonJPEG.TabIndex = 2
+        RadioButtonJPEG.TabStop = True
+        RadioButtonJPEG.Text = ".jpg"
+        RadioButtonJPEG.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButtonPNG
+        ' 
+        RadioButtonPNG.AutoSize = True
+        RadioButtonPNG.Cursor = Cursors.Hand
+        RadioButtonPNG.Location = New Point(117, 204)
+        RadioButtonPNG.Name = "RadioButtonPNG"
+        RadioButtonPNG.Size = New Size(49, 19)
+        RadioButtonPNG.TabIndex = 1
+        RadioButtonPNG.TabStop = True
+        RadioButtonPNG.Text = ".png"
+        RadioButtonPNG.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonBrowse
+        ' 
+        ButtonBrowse.Cursor = Cursors.Hand
+        ButtonBrowse.Location = New Point(362, 251)
+        ButtonBrowse.Name = "ButtonBrowse"
+        ButtonBrowse.Size = New Size(88, 23)
+        ButtonBrowse.TabIndex = 4
+        ButtonBrowse.Text = "Browse File.."
+        ButtonBrowse.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonConvert
+        ' 
+        ButtonConvert.Cursor = Cursors.Hand
+        ButtonConvert.Location = New Point(12, 308)
+        ButtonConvert.Name = "ButtonConvert"
+        ButtonConvert.Size = New Size(438, 23)
+        ButtonConvert.TabIndex = 5
+        ButtonConvert.Text = "Convert"
+        ButtonConvert.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(8, 255)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(64, 15)
+        Label2.TabIndex = 6
+        Label2.Text = "image list :"
+        ' 
+        ' TextBoxOutputPath
+        ' 
+        TextBoxOutputPath.Location = New Point(78, 279)
+        TextBoxOutputPath.Name = "TextBoxOutputPath"
+        TextBoxOutputPath.ReadOnly = True
+        TextBoxOutputPath.Size = New Size(372, 23)
+        TextBoxOutputPath.TabIndex = 9
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(23, 282)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(49, 15)
+        Label3.TabIndex = 10
+        Label3.Text = "output :"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(458, 337)
+        Controls.Add(Label3)
+        Controls.Add(TextBoxOutputPath)
+        Controls.Add(Label2)
+        Controls.Add(ButtonConvert)
+        Controls.Add(ButtonBrowse)
+        Controls.Add(GroupBox1)
+        Controls.Add(TextBoxInputPath)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximumSize = New Size(474, 376)
+        MinimumSize = New Size(474, 376)
+        Name = "Form1"
+        Text = "DDS Converter By Kimparkz"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBoxStatus As TextBox
+    Friend WithEvents TextBoxInputPath As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RadioButtonJPEG As RadioButton
+    Friend WithEvents RadioButtonPNG As RadioButton
+    Friend WithEvents ButtonBrowse As Button
+    Friend WithEvents ButtonConvert As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxOutputPath As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ TR-DDS by Kimparkz/Form1.vb
@@ -0,0 +1,92 @@
+﻿Imports System.IO
+Imports FreeImageAPI
+
+'Hi, this program is a Kimparkz's project
+'Welcome to my code -w-
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'config
+        RadioButtonPNG.Checked = True
+        Me.AllowDrop = True
+        RadioButtonPNG.Checked = True
+        TextBoxStatus.Multiline = True
+        OpenFileDialog1.Multiselect = True
+        'other
+        AddHandler Me.DragEnter, AddressOf Form1_DragEnter
+        AddHandler Me.DragDrop, AddressOf Form1_DragDrop
+        TextBoxStatus.ScrollBars = ScrollBars.Vertical
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs)
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs)
+        Dim files As String() = DirectCast(e.Data.GetData(DataFormats.FileDrop), String())
+
+        If files.Length > 0 Then
+            Dim ddsFiles = files.Where(Function(file) Path.GetExtension(file).ToLower() = ".dds").ToList()
+
+            If ddsFiles.Count > 0 Then
+                Dim fileNames = ddsFiles.Select(Function(file) Path.GetFileName(file))
+                TextBoxInputPath.Text = String.Join(", ", fileNames)
+                For Each filePath As String In ddsFiles
+                    ConvertDDSFile(filePath)
+                Next
+            Else
+                TextBoxStatus.Text = "No .dds files found."
+            End If
+        End If
+    End Sub
+
+    Private Sub ButtonBrowse_Click(sender As Object, e As EventArgs) Handles ButtonBrowse.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim selectedFiles As String() = OpenFileDialog1.FileNames
+            TextBoxInputPath.Text = String.Join(Environment.NewLine, selectedFiles)
+        End If
+    End Sub
+
+    Private Sub ButtonConvert_Click(sender As Object, e As EventArgs) Handles ButtonConvert.Click
+        Dim selectedFiles As String() = TextBoxInputPath.Lines
+        For Each filePath As String In selectedFiles
+            If Not String.IsNullOrWhiteSpace(filePath) Then
+                ConvertDDSFile(filePath)
+            End If
+        Next
+    End Sub
+
+    Private Sub ConvertDDSFile(inputPath As String)
+        If File.Exists(inputPath) Then
+            Dim fileName As String = Path.GetFileName(inputPath)
+            Dim image As FIBITMAP = FreeImage.Load(FREE_IMAGE_FORMAT.FIF_DDS, inputPath, FREE_IMAGE_LOAD_FLAGS.DEFAULT)
+
+            If Not IsNothing(image) Then
+                Dim outputFormat As FREE_IMAGE_FORMAT = If(RadioButtonPNG.Checked, FREE_IMAGE_FORMAT.FIF_PNG, FREE_IMAGE_FORMAT.FIF_JPEG)
+                Dim outputExtension As String = If(outputFormat = FREE_IMAGE_FORMAT.FIF_PNG, "png", "jpg")
+                Dim outputFolder As String = Path.GetDirectoryName(inputPath)
+                Dim outputSubFolder As String = Path.Combine(outputFolder, "dds_output")
+                Directory.CreateDirectory(outputSubFolder)
+
+                Dim outputFileName As String = Path.GetFileNameWithoutExtension(inputPath)
+                Dim outputPath As String = Path.Combine(outputSubFolder, $"{outputFileName}.{outputExtension}")
+
+                Try
+                    FreeImage.Save(outputFormat, image, outputPath, FREE_IMAGE_SAVE_FLAGS.DEFAULT)
+                    FreeImage.Unload(image)
+                    TextBoxOutputPath.Text = Path.GetDirectoryName(outputPath)
+                    TextBoxStatus.AppendText($"Process : {fileName} → Convert Success" & Environment.NewLine)
+                Catch ex As Exception
+                    TextBoxStatus.AppendText($"An error occurred during conversion for {fileName}: {ex.Message}" & Environment.NewLine)
+                End Try
+            Else
+                TextBoxStatus.AppendText($"Input file {inputPath} does not exist." & Environment.NewLine)
+            End If
+        Else
+            TextBoxStatus.AppendText($"Input file does not exist." & Environment.NewLine)
+        End If
+    End Sub
+End Class
